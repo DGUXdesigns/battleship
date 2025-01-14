d08593b9713ebd3bb5d420b4b94ba98c9d969848
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-01-13 22:40:29 -0500

    test: add unit tests for hits increment in `Ship` class

--- src/scripts/ship.test.js
+++ src/scripts/ship.test.js
@@ -2,21 +2,31 @@ import { describe, expect, it, test } from '@jest/globals';
 import { Ship } from './ship';
 
 describe('Ship Class', () => {
-    const result = new Ship();
+    const ship = new Ship(3);
 
     it('Should be an object', () => {
-        expect(result).toBeInstanceOf(Object);
+        expect(ship).toBeInstanceOf(Object);
     });
 
     it('Should contain ship length', () => {
-        expect(result).toHaveProperty('length');
+        expect(ship).toHaveProperty('length');
     });
 
     it("Should show the number of times they've been hit", () => {
-        expect(result).toHaveProperty('hits');
+        expect(ship).toHaveProperty('hits');
     });
 
     it('Should show if ship has sunk', () => {
-        expect(result).toHaveProperty('sunk');
+        expect(ship).toHaveProperty('sunk');
+    });
+
+    test('Hits should increment by hits count', () => {
+        expect(ship.hits).toBe(0);
+
+        ship.hit(); // Use renamed method
+        expect(ship.hits).toBe(1);
+
+        ship.hit(); // Use renamed method
+        expect(ship.hits).toBe(2);
     });
 });
