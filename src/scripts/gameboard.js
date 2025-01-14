7a676a40964b25d2e09ae0b26dd1294281454c15
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-01-14 13:07:40 -0500

    feat: update ship placement to check validity and add ships to board

--- src/scripts/gameboard.js
+++ src/scripts/gameboard.js
@@ -2,6 +2,7 @@ export class Gameboard {
   constructor(size) {
     this.size = size;
     this.board = this.buildBoard(this.size);
+    this.ships = [];
   }
 
   buildBoard(size) {
@@ -15,14 +16,34 @@ export class Gameboard {
   }
 
   placeShipHorizontal(ship, row, col) {
+    if (col + ship.length > this.size) {
+      throw new Error('Cannot place ship here');
+    }
+
     for (let i = 0; i < ship.length; i++) {
+      if (this.board[row][col + i] === 1) {
+        throw new Error('Ships cannot overlap');
+      }
+
       this.board[row][col + i] = 1;
     }
+
+    this.ships.push(ship);
   }
 
   placeShipVertical(ship, row, col) {
+    if (row + ship.length > this.size) {
+      throw new Error('Cannot place ship here');
+    }
+
     for (let i = 0; i < ship.length; i++) {
+      if (this.board[row + i][col] === 1) {
+        throw new Error('Ships cannot overlap');
+      }
+
       this.board[row + i][col] = 1;
     }
+
+    this.ships.push(ship);
   }
 }
