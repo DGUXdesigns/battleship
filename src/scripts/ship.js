71877a1032f575387f66df3964b74122d95a37c2
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-01-14 16:44:57 -0500

    feat: update hit() to check if the ship is sunk after each hit

--- src/scripts/ship.js
+++ src/scripts/ship.js
@@ -1,17 +1,18 @@
 export class Ship {
-    constructor(length) {
-        this.length = length;
-        this.hits = 0;
-        this.sunk = false;
-    }
+  constructor(length) {
+    this.length = length;
+    this.hits = 0;
+    this.sunk = false;
+  }
 
-    hit() {
-        this.hits++;
-    }
+  hit() {
+    this.hits++;
+    this.isSunk();
+  }
 
-    isSunk() {
-        if (this.hits === this.length) {
-            this.sunk = true;
-        }
+  isSunk() {
+    if (this.hits === this.length) {
+      this.sunk = true;
     }
+  }
 }
