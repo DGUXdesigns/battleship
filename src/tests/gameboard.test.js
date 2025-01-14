498d09aa06a558bf3bac7651820c7af249083669
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-01-14 12:28:29 -0500

    refactor: organize tests into separate describe blocks for clarity

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,13 +1,13 @@
 import { describe, expect, it, test, beforeEach } from '@jest/globals';
 import { Gameboard } from '../scripts/gameboard';
 
+let board;
+
+beforeEach(() => {
+  board = new Gameboard(10);
+});
+
 describe('Gameboard', () => {
-  let board;
-
-  beforeEach(() => {
-    board = new Gameboard(10);
-  });
-
   it('should be an Object', () => {
     expect(board).toBeInstanceOf(Object);
   });
@@ -20,14 +20,17 @@ describe('Gameboard', () => {
       expect(row.length).toBe(10);
     });
   });
+});
+
+describe('ship placement', () => {
+  const ship = { length: 5 };
 
   test('placeShipHorizontal places a ship horizantally on the board', () => {
-    const ship = { length: 3 };
     board.placeShipHorizontal(ship, 1, 1);
 
     const expectedBoard = [
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+      [0, 1, 1, 1, 1, 1, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -42,7 +45,6 @@ describe('Gameboard', () => {
   });
 
   test('placeShipVertical places a ship vertically on the board', () => {
-    const ship = { length: 5 };
     board.placeShipVertical(ship, 1, 1);
 
     const expectedBoard = [
@@ -61,19 +63,15 @@ describe('Gameboard', () => {
     expect(board.board).toEqual(expectedBoard);
   });
 
-  describe('Out-of-bounds ship placement', () => {
-    const ship = { length: 5 };
+  test('Horizontal placement should not allow out-of-bounds', () => {
+    expect(() => {
+      board.placeShipHorizontal(ship, 9, 8);
+    }).toThrowError('Cannot place ship here');
+  });
 
-    test('Horizontal placement should not allow out-of-bounds', () => {
-      expect(() => {
-        board.placeShipHorizontal(ship, 7, 8);
-      }).toThrowError('Cannot place ship here');
-    });
-
-    test('Vertical placement should not allow out-of-bounds', () => {
-      expect(() => {
-        board.placeShipVertical(ship, 7, 8);
-      }).toThrowError('Cannot place ship here');
-    });
+  test('Vertical placement should not allow out-of-bounds', () => {
+    expect(() => {
+      board.placeShipVertical(ship, 9, 0);
+    }).toThrowError('Cannot place ship here');
   });
 });
