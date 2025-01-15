92b902a85c2ff50ff307c602d989d9de09f4cf22
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-01-14 21:18:50 -0500

    feat: add Player class with attack method and player type logic

---
+++ src/scripts/player.js
@@ -0,0 +1,22 @@
+import { Gameboard } from './gameboard';
+
+export class Player {
+  constructor(player) {
+    this.playerType = this.isComputer(player);
+    this.gameboard = new Gameboard(10);
+  }
+
+  isComputer(player) {
+    return player === 'computer' ? 'computer' : 'human';
+  }
+
+  attack(gameboard, row, col) {
+    if (this.playerType === 'computer') {
+      row = Math.floor(Math.random() * 10);
+      col = Math.floor(Math.random() * 10);
+      return gameboard.receiveAttack(row, col);
+    }
+
+    return gameboard.receiveAttack(row, col);
+  }
+}
