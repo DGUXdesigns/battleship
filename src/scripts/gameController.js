c21295809d513fd637a3162175c90bc9f4e81c4d
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-01-16 01:54:53 -0500

    feat: add feedback for errors

--- src/scripts/gameController.js
+++ src/scripts/gameController.js
@@ -12,17 +12,17 @@ export class GameController {
   startGame() {
     //TODO: Iplement system for allowing players to place their ships
     const ships = [
-      { name: 'Carrier', length: 5, row: 2, col: 2, direction: 'vertical' },
+      { name: 'Carrier', length: 5, row: 0, col: 0, direction: 'vertical' },
       {
         name: 'Battleship',
         length: 4,
-        row: 0,
-        col: 0,
+        row: 2,
+        col: 5,
         direction: 'horizontal',
       },
-      { name: 'Cruiser', length: 3, row: 2, col: 7, direction: 'vertical' },
-      { name: 'Submarine', length: 3, row: 8, col: 1, direction: 'horizontal' },
-      { name: 'Destroyer', length: 2, row: 7, col: 5, direction: 'horizontal' },
+      { name: 'Cruiser', length: 3, row: 6, col: 2, direction: 'vertical' },
+      { name: 'Submarine', length: 3, row: 4, col: 7, direction: 'horizontal' },
+      { name: 'Destroyer', length: 2, row: 8, col: 5, direction: 'horizontal' },
     ];
 
     this.populateShips(this.playerOne, ships);
@@ -32,7 +32,11 @@ export class GameController {
   populateShips(player, ships) {
     ships.forEach(({ name, length, row, col, direction }) => {
       const ship = new Ship(name, length);
-      player.gameboard.placeShip(ship, row, col, direction);
+      try {
+        player.gameboard.placeShip(ship, row, col, direction);
+      } catch (error) {
+        alert(`Error placing ${name}: ${error.message}`);
+      }
     });
   }
 }
