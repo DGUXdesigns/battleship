33194639b4c52cde4c428ac99f5bc4aa612eb895
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-01-17 16:48:41 -0500

    feat: initialize players, place ships, and render game in index.js
    - Added initialization of Player instances for player1 (human) and player2 (AI).
    - Implemented ship creation and placement on both players' gameboards.
    - Integrated `GameUI` to render game boards and start the game.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './stylesheet.css';
-import { Ship } from './scripts/ship';
-import { Gameboard } from './scripts/gameboard';
 import { Player } from './scripts/player';
+import { Ship } from './scripts/ship';
 import { StartScreen } from './displayLogic/renderStart';
 import { setError } from './displayLogic/formHandling';
+import { GameUI } from './displayLogic/renderGame';
 
 const startScreen = new StartScreen('main');
 
@@ -19,11 +19,34 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     const playerName = name.value.trim();
-
-    const player = new Player(playerName);
-    const AI = new Player('computer');
-
     name.value = '';
+
+    // Initialize players
+    const player1 = new Player(playerName);
+    const player2 = new Player('computer');
+
+    // Place ships on the boards
+    const carrier = new Ship('Carrier', 5);
+    const battleship = new Ship('Battleship', 4);
+    const cruiser = new Ship('Cruise', 4);
+    const submarine = new Ship('Submarine', 3);
+    const destroyer = new Ship('destroyer', 3);
+
+    // Place ships on the board
+    player1.gameboard.placeShip(carrier, 0, 0, 'vertical');
+    player1.gameboard.placeShip(battleship, 2, 5, 'horizontal');
+    player1.gameboard.placeShip(cruiser, 6, 2, 'vertical');
+    player1.gameboard.placeShip(submarine, 4, 7, 'horizontal');
+    player1.gameboard.placeShip(destroyer, 8, 5, 'horizontal');
+
+    player2.gameboard.placeShip(carrier, 0, 0, 'vertical');
+    player2.gameboard.placeShip(battleship, 2, 5, 'horizontal');
+    player2.gameboard.placeShip(cruiser, 6, 2, 'vertical');
+    player2.gameboard.placeShip(submarine, 4, 7, 'horizontal');
+    player2.gameboard.placeShip(destroyer, 8, 5, 'horizontal');
+
+    const render = new GameUI('main', player1, player2);
+    render.initGame();
   });
 });
 
