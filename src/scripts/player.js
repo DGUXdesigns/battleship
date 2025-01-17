ac3f1b8598b30f1693796572c3616af2b4e1c277
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-01-17 16:45:28 -0500

    chore: change uppercase methos name to 'capitalize'

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -2,7 +2,7 @@ import { Gameboard } from './gameboard';
 
 export class Player {
   constructor(player) {
-    this.playerName = this.upperCase(player);
+    this.playerName = this.capitalize(player);
     this.playerType = this.isComputer(player);
     this.gameboard = new Gameboard(10);
   }
@@ -15,13 +15,15 @@ export class Player {
     if (this.playerType === 'computer') {
       row = Math.floor(Math.random() * 10);
       col = Math.floor(Math.random() * 10);
-      return gameboard.receiveAttack(row, col);
+      if (gameboard.receiveAttack(row, col).hit !== null) {
+        return;
+      }
     }
 
     return gameboard.receiveAttack(row, col);
   }
 
-  upperCase(name) {
+  capitalize(name) {
     return name.charAt(0).toUpperCase() + name.slice(1);
   }
 }
